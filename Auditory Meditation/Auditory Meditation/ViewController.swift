998542346b5b5828efa49c58421890e037b509df
zachsilv <zachsilv@gmail.com>
2015-02-07 08:38:49 -0700

    First push

---
+++ Auditory Meditation/Auditory Meditation/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Auditory Meditation
+//
+//  Created by Zach Silverman on 2/7/15.
+//  Copyright (c) 2015 ZAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var First_Image: UIImageView!
+    @IBOutlet weak var Second_Image: UIImageView!
+    @IBOutlet weak var Third_Image: UIImageView!
+    @IBOutlet weak var Fourth_Image: UIImageView!
+    @IBOutlet weak var Grid_View: UIView!
+    @IBOutlet weak var App_Name: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
